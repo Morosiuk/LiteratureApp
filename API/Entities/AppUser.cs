9a5e9df94a6d080f2363a22e02270992f2eaee01
XAIS-Karl\Karl Morosiuk <karl.morosiuk@xais.co.uk>
2021-12-28 13:16:51 +0000

    Add authentication.

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -4,7 +4,8 @@ namespace API.Entities
     {
         public int Id { get; set; }   
         public string UserName { get; set; }
-        
+        public byte[] Password { get; set; }
+        public byte[] PasswordSalt { get; set; }        
 
     }
 }
