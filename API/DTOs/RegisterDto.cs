d9eabb692253501814aa836184221cd1830a2881
XAIS-Karl\Karl Morosiuk <karl.morosiuk@xais.co.uk>
2021-12-30 17:27:44 +0000

    Add api middleware.

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -9,6 +9,7 @@ namespace API.DTOs
         public string Username { get; set; }
         
         [Required]
+        [StringLength(12, MinimumLength = 6)]
         public string Password { get; set; }
     }
 }
